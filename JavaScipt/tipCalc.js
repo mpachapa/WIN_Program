2d8504cdb133a6faa91944722765e1586e577924
Matthew <mpachapa@uncc.edu>
2022-03-22 11:52:58 -0400

    updating

--- JavaScipt/tipCalc.js
+++ JavaScipt/tipCalc.js
@@ -6,7 +6,7 @@ function calcTipWTotal(total) {
     return total * 1.2;
 }
 
-let billTotal = 100;
+let billTotal = 159;
 console.log(calcTip(200));
 console.log(calcTipWTotal(200) + "\n");
 console.log(calcTip(billTotal));
---
+++ JavaScipt/userEmail.js
@@ -0,0 +1,6 @@
+var emailApplication = {
+    name: Gmail,
+    mailboxes: ['inbox', 'spam', 'promotions', 'sent', 'drafts'],
+    contacts: [{ name: 'Matt', email: 'matt@carolinafintechhub.org' }, { name: 'Julian', email: 'julian@carolinafintechhub.org' }, { name: 'Erin', email: 'erin@carolinafintechhub.org' }, { name: 'Hector', email: 'hector@carolinafintechhub.org' }],
+
+}
